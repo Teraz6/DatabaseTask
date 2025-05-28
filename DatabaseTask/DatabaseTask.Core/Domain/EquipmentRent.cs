9cf004c62ecae9e39c58590500feabcda4563bff
Teraz6 <retenk@gmail.com>
2025-05-28 13:38:26 +0300

    Create EquipmentRent.cs

---
+++ DatabaseTask/DatabaseTask.Core/Domain/EquipmentRent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTask.Core.Domain
+{
+    class EquipmentRent
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        [ForeignKey(nameof(EmployeeId))]
+        public Employee Employee { get; set; }
+
+        public int DeviceId { get; set; }
+        [ForeignKey(nameof(DeviceId))]
+        public Devices Devices { get; set; }
+        
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
