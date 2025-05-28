504afd2553bf21eeffb253e3c3b5df9a40d89e9a
Teraz6 <retenk@gmail.com>
2025-05-28 13:29:24 +0300

    Create JobHistory.cs

---
+++ DatabaseTask/DatabaseTask.Core/Domain/JobHistory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTask.Core.Domain
+{
+    class JobHistory
+    {
+        public int Id { get; set; }
+
+        public int PositionId { get; set; }
+        [ForeignKey(nameof(PositionId))]
+        public Position Position { get; set; }
+
+        public int EmployeeId { get; set; }
+        [ForeignKey(nameof(EmployeeId))]
+        public Employee Employee { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
