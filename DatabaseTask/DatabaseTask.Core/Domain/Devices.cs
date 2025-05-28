10532cc98d2d965cbec0b66f929bd4006be24c22
Teraz6 <retenk@gmail.com>
2025-05-28 13:38:24 +0300

    Create Devices.cs

---
+++ DatabaseTask/DatabaseTask.Core/Domain/Devices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTask.Core.Domain
+{
+    class Devices
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Manufacturer { get; set; }
+        public string? Type { get; set; }
+        public string SerialNumber { get; set; }
+        public string Condition { get; set; }
+    }
+}
