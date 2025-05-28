205ca1991d1028bda9baa74aad4f416ac961a9b4
Teraz6 <retenk@gmail.com>
2025-05-28 13:38:46 +0300

    Fix typo in Address.cs

--- DatabaseTask/DatabaseTask.Core/Domain/Address.cs
+++ DatabaseTask/DatabaseTask.Core/Domain/Address.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DatabaseTask.Core.Domain
 {
-    class Aadress
+    class Address
     {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        [ForeignKey(nameof(EmployeeId))]
+        public Employee Employee { get; set; }
+
+        public string PostalCode { get; set; }
+        public int AppartmentNr { get; set; }
+        public int BuildingNr { get; set; }
+        public string Street { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string Country { get; set; }
     }
 }
