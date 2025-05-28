eb28af6fea49978fcd0361dd66a2625f8d5e7e15
Teraz6 <retenk@gmail.com>
2025-05-28 13:29:22 +0300

    Create Position.cs

---
+++ DatabaseTask/DatabaseTask.Core/Domain/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTask.Core.Domain
+{
+    class Position
+    {
+        public int Id { get; set; }
+        public int AccessId { get; set; }
+        [ForeignKey(nameof(AccessId))]
+        public Access Access { get; set; }
+        public string Description { get; set; }
+        public DateTime PositionCreatedDate { get; set; }
+        public DateTime PositionDisabledDate { get; set;
+    }
+}
