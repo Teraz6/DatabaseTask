b5b8e27b39d5b98894a2b84dc6009116c962c9f2
Teraz6 <retenk@gmail.com>
2025-05-28 13:29:19 +0300

    Create Salary.cs

---
+++ DatabaseTask/DatabaseTask.Core/Domain/Salary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTask.Core.Domain
+{
+    class Salary
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        [ForeignKey(nameof(EmployeeId))]
+        public Employee Employee { get; set; }
+        public int Wage {  get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
