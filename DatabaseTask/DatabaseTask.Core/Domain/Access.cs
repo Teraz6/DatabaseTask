63c82ae271352a602fc6b3623b422cdd57dafb41
Teraz6 <retenk@gmail.com>
2025-05-28 13:29:26 +0300

    Create Access.cs

---
+++ DatabaseTask/DatabaseTask.Core/Domain/Access.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTask.Core.Domain
+{
+    class Access
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
